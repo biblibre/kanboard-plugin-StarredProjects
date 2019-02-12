b628cb106297cb345c2f48d076c403f88f6e8020
Julian Maurice <julian.maurice@biblibre.com>
2019-02-12 09:49:46 +0100

    Fix schema for sqlite

--- Schema/Sqlite.php
+++ Schema/Sqlite.php
@@ -9,8 +9,8 @@ const VERSION = 1;
 function version_1(PDO $pdo)
 {
     $pdo->exec('
-        CREATE TABLE IF NOT EXISTS metadata_types (
-          id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        CREATE TABLE IF NOT EXISTS starred_projects (
+          id INTEGER PRIMARY KEY,
           user_id INT(11) NOT NULL,
           project_id INT(11) NOT NULL,
           UNIQUE(user_id, project_id)
