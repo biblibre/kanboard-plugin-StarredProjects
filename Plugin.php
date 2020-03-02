cbf22f2bc934c69c077262497bd6cdf2b1794c87
Julian Maurice <julian.maurice@biblibre.com>
2020-03-02 18:16:44 +0100

    Add a user setting to show/hide non-favorite projects in project switcher
    This setting is accessible by going to 'My Profile > Integrations'

--- Plugin.php
+++ Plugin.php
@@ -9,6 +9,7 @@ class Plugin extends Base
 {
     public function initialize()
     {
+        $this->template->hook->attach('template:user:integrations', 'StarredProjects:user/integrations');
         $this->template->setTemplateOverride('dashboard/overview', 'StarredProjects:dashboard/overview');
         $this->template->setTemplateOverride('header/board_selector', 'StarredProjects:header/board_selector');
         $this->hook->on('template:layout:js', array('template' => 'plugins/StarredProjects/assets/js/starred-projects.js'));
--- Template/header/board_selector.php
+++ Template/header/board_selector.php
@@ -1,9 +1,12 @@
 <?php
     $starred_projects = $this->app->starredProjectsModel->findAllProjects($this->user->getId());
-    foreach ($starred_projects as $project) {
-        $id = $project['id'];
-        if (array_key_exists($id, $board_selector)) {
+    $starred_projects = array_column($starred_projects, 'name', 'id');
+    $only_starred = $this->app->userMetadataModel->get($this->user->getId(), 'starredprojects_only_starred_in_project_selector', '0');
+    foreach ($board_selector as $id => $project) {
+        if (array_key_exists($id, $starred_projects)) {
             $board_selector[$id] = " ★ " . $board_selector[$id];
+        } elseif ($only_starred) {
+            unset($board_selector[$id]);
         }
     }
 ?>
