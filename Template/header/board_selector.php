e512f8981152dfc349c8299c916a92c21f732827
Alfred Bühler <abu@alfredbuehler.ch>
2023-02-13 14:00:47 +0100

    Extend the original template rather than overwrite it

--- Template/header/board_selector.php
+++ Template/header/board_selector.php
@@ -10,16 +10,4 @@
         }
     }
 ?>
-
-<?= $this->app->component('select-dropdown-autocomplete', array(
-    'name' => 'boardId',
-    'placeholder' => t('Display another project'),
-    'items' => $board_selector,
-    'redirect' => array(
-        'regex' => 'PROJECT_ID',
-        'url' => $this->url->to('BoardViewController', 'show', array('project_id' => 'PROJECT_ID')),
-    ),
-    'onFocus' => array(
-        'board.selector.open',
-    )
-)) ?>
+<?= $this->render('kanboard:header/board_selector', array('board_selector' => $board_selector)) ?>
