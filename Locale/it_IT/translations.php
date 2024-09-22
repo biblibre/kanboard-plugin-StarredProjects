05a51d3f656b2060bdb0965114a92f541da2fe72
kenlog <valentino@iltuobrand.it>
2024-09-22 15:37:27 +0200

    Adds Italian translation and improves UI Integrations

---
+++ Locale/it_IT/translations.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'Starred projects' => 'Progetti preferiti',
+    'Show only starred projects in project selector' => 'Mostra solo i progetti contrassegnati con una stella nel selettore progetti',
+];
--- Template/user/integrations.php
+++ Template/user/integrations.php
@@ -1,10 +1,8 @@
-<h3>Starred Projects</h3>
-
-<div class="panel">
+<fieldset>
+    <legend><?= t('Starred projects') ?></legend>
     <input type="hidden" name="starredprojects_only_starred_in_project_selector" value="0">
     <?php echo $this->form->checkbox('starredprojects_only_starred_in_project_selector', t('Show only starred projects in project selector'), 1, $values['starredprojects_only_starred_in_project_selector']); ?>
-
     <div class="form-actions">
         <button type="submit" class="btn btn-blue"><?php echo t('Save'); ?></button>
     </div>
-</div>
+</fieldset>
